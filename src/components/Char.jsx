f808f3023d06e26747faa054b4d66e3f744d0864
MustafaWael <mustafa.wael.dev@gmail.com>
2023-04-24 23:47:21 +0200

    1 file modified

--- src/components/Char.jsx
+++ src/components/Char.jsx
@@ -1,24 +1,18 @@
-import { useScroll, useTransform, motion } from 'framer-motion';
+import { useScroll, useTransform, motion, useInView } from 'framer-motion';
 
 const Char = ({ wrapperRef, children, index, charactersLength }) => {
   const { scrollYProgress } = useScroll({ target: wrapperRef });
-  const startingPosition = index / (charactersLength + 1);
-  const endingPosition = (index + 1) / (charactersLength + 1);
+  const startingPosition = index / (charactersLength + 5);
+  const endingPosition = (index + 5) / (charactersLength + 5);
   const opacity = useTransform(
     scrollYProgress,
     [startingPosition, endingPosition],
     [0.2, 1]
   );
 
-  return (
-    <motion.span
-      style={{
-        opacity,
-      }}
-    >
-      {children}
-    </motion.span>
-  );
+  const inView = useInView(wrapperRef)
+
+  return <motion.span style={{ opacity }}>{inView && children}</motion.span>;
 };
 
 export default Char;
